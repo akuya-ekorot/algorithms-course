14b643002f8be67577f77a56b99ac58bba141cf7
Akuya Ekorot <akuyaekorot@gmail.com>
2024-04-11 13:32:17 +0300

    organize code into lessons folder

--- tests/big-o/0-linear_search.test.ts
+++ tests/big-o/0-linear_search.test.ts
@@ -1,8 +1,8 @@
-import linearSearch from '../../big-o/0-linear_search.ts';
-import { describe, expect, test } from 'bun:test'
+import { describe, expect, test } from "bun:test";
+import linearSearch from "../../lessons/big-o/0-linear_search";
 
-describe('linearSearch', () => {
-  test('finds needle in haystack', () => {
+describe("linearSearch", () => {
+  test("finds needle in haystack", () => {
     const hayStack = [0, 3, 1, 2, 6];
     expect(linearSearch(hayStack, 2)).toBe(3);
   });
